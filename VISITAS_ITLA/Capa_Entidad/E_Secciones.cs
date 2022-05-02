bd2f6bda79e5da3817720860fa9a8aabb42e574d
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2022-05-01 21:54:38 -0700

    Create E_Secciones.cs

---
+++ VISITAS_ITLA/Capa_Entidad/E_Secciones.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class E_Secciones
+    {
+        private int _IdSecciones;
+        private string _Nombre;
+        private int _NumeroEdifico;
+        private string _NombreNuevo;
+
+        public int IdSecciones { get => _IdSecciones; set => _IdSecciones = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public int NumeroEdifico { get => _NumeroEdifico; set => _NumeroEdifico = value; }
+        public string NombreNuevo { get => _NombreNuevo; set => _NombreNuevo = value; }
+    }
+}
