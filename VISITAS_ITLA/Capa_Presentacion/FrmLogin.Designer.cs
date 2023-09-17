92b29ed3cd89771f9a980aa9deb644cad3a40363
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2023-09-17 18:25:28 -0400

    Update FrmLogin.Designer.cs

--- VISITAS_ITLA/Capa_Presentacion/FrmLogin.Designer.cs
+++ VISITAS_ITLA/Capa_Presentacion/FrmLogin.Designer.cs
@@ -82,6 +82,7 @@
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 1;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // txtUsuario
             // 
